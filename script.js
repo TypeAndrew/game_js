c76677b71ca17d48819e0b5448a6ad2981c9b6b6
TypeAndrew <andy.shular@gmail.com>
2023-03-21 15:11:57 +0200

    game js

--- script.js
+++ script.js
@@ -7,9 +7,31 @@ playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 let framex = 0;
-let frameY = 5;
+let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
+const spriteAnimation = [];
+const animationStates = [{
+    mame: 'idle',
+    farmes: 7,
+}, {
+    name: 'jump',
+    farmes: 7,
+}];
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    };
+    console.log(frames);
+    for (let j = 0; j < state.frames; s++) {
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({ x: positionX, y: positionY });
+    }
+    spriteAnimation[state.name] = frames;
+})
+console.log(animationStates);
+
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
